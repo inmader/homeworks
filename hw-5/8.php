56485f4547571c4761b47394fbd2c7490fb9df3e
inmader <inmader@mail.ru>
2016-12-12 16:35:09 +0200

    Update 8.php

--- hw-5/8.php
+++ hw-5/8.php
@@ -8,38 +8,35 @@ $arr_delete = ['.',',','!','/'];
 $str = $_POST['commit'];
 
 if (!empty($_POST['name']) and !empty($_POST['commit'])) {
-
     $arr = explode(' ',$str);
     foreach ($arr as $key => $value ){
         if (in_array($value,$forbiden_arr)){
-            echo ('<h1>Uncorect words or symbols!<br></h1>'); // forbidden word
+           echo ('<h1>Forbidden words!<br></h1>'); // forbidden words
+            $hidden = '<h1>Forbidden words!<br></h1>';
 
         }
     }
-
-    $arr2 = str_split($str);
-    foreach ($arr2 as $key2 => $value2 ){
-        if (in_array($value2,$arr_delete)){
-            unset($arr2[$key2]);  // delete symbols
-
+        $arr2 = str_split($str);
+        foreach ($arr2 as $key2 => $value2) {
+            if (in_array($value2, $arr_delete)) {
+                unset($arr2[$key2]);  // delete symbols
+            }
         }
-    }
+        $str = implode('', $arr2);
+        $commit = $_POST['name'] . ':' . $str . '<br>';
 
-
-    $str = implode('',$arr2);
-    $commit = $_POST['name'] . ':' . $str . '<br>';
-    file_put_contents($file, $commit, FILE_APPEND);
-
-    $data=file_get_contents($file);
-    echo $data;
+        if (!isset($hidden)) {
+            file_put_contents($file, $commit, FILE_APPEND);
+        }
+    
+        $data = file_get_contents($file);
+        echo $data;
 
 }
 else{
     echo ('<h1>Wrong information!<br></h1>');
-
 }
-
-if (!in_array($value,$forbiden_arr)) {
+if (!isset($hidden)) {
     $html = '<br>
     <form method="POST" enctype="multipart/form-data">
         <input type="text" name="name"><br>
@@ -47,4 +44,5 @@ if (!in_array($value,$forbiden_arr)) {
         <input type="submit" value="Add">
     </form>';
     echo $html;
+
 }
