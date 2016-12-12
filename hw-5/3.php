2a32cfb335831ee463155dff843dd987193c0e4c
inmader <inmader@mail.ru>
2016-12-12 09:29:50 +0200

    Update 3.php

--- hw-5/3.php
+++ hw-5/3.php
@@ -1,31 +1,29 @@
-
 <?php
 
 $form1 = '<form name="myform" method="post">
 <textarea cols="100" rows="10" name="text1" method="post"></textarea><br>
 <input type="submit" />
 </form>';
-
 echo $form1;
 
 $dir2='D:/test/test.txt';
-
 
 function word_length($dir)
 {
     $N=$_POST['text1'];
     $data = file_get_contents($dir);
     $arr = explode(' ', $data);
-    $str = '';
-    for ($i = 0; $i < count($arr); $i++) {
-        if (strlen($arr[$i]) > $N) {
-            $str .= $arr[$i] . '<br>';
+    $data2 = '';
+    foreach ($arr as $key => $value) {
+
+        if (strlen($value) > $N) {
+            unset($arr[$key]);
+
         }
-
     }
-    echo '<pre>';
-    print_r($str);
-    echo '</pre>';
-}
+    $data2 = implode(',',$arr);
+    file_put_contents($dir,$data2);
 
+
+}
 echo word_length($dir2);
