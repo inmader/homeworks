7b6f1bc68a6cb10c0382dc7ed9c0532d5525ff5d
inmader <inmader@mail.ru>
2016-12-12 14:51:42 +0200

    Update 8.php

--- hw-5/8.php
+++ hw-5/8.php
@@ -1,15 +1,16 @@
-
 <?php
 
-$file = 'book.txt';
+$file = 'D:\test\test.txt';
+
+$forbiden_arr = ['.',',','!','/' ,'forbiden'];
 
 if (!empty($_POST['name']) and !empty($_POST['commit'])) {
 
-
     $arr = explode(' ',$_POST['commit']);
     foreach ($arr as $key => $value ){
-        if ($value =='forbiden'){
-           header('Location: /info.php'); // forbidden word
+        if (in_array($value,$forbiden_arr)){
+            echo ('<h1>Uncorect words or symbols!<br></h1>'); // forbidden word
+            $html='';
         }
     }
     $_POST['commit'] = implode(' ',$arr);
@@ -17,18 +18,21 @@ if (!empty($_POST['name']) and !empty($_POST['commit'])) {
     $commit = $_POST['name'] . ':' . $_POST['commit'] . '<br>';
     file_put_contents($file, $commit, FILE_APPEND);
 
-}else{
-    header('Location: /info.php'); // message about wrong information
+    $data=file_get_contents($file);
+    echo $data;
+
+}
+else{
+    echo ('<h1>Wrong information!<br></h1>');
+
 }
 
-
-$data=file_get_contents($file);
-echo $data;
-
-$html = '<br>
+if (!in_array($value,$forbiden_arr)) {
+    $html = '<br>
     <form method="POST" enctype="multipart/form-data">
         <input type="text" name="name"><br>
-        <input type="text" name="commit"><br>
+        <textarea cols="100" rows="10" name="commit" method="post"></textarea><br>
         <input type="submit" value="Add">
     </form>';
-echo $html;
+    echo $html;
+}
